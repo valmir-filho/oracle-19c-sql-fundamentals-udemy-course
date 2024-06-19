b8c20f45986e8a0bb85e5b595ca0a81090f797b4
Valmir <vmcfilho@gmail.com>
2024-06-19 19:10:31 -0300

    Update 01.sql

--- 01.sql
+++ 01.sql
@@ -1,7 +1,24 @@
 -- Describe the table.
 DESC employess;
 
--- Basic select.
+-- Basic selects.
 SELECT *
 FROM employess;
 
+SELECT
+    department_id, location_id
+FROM departments;
+
+SELECT
+    first_name,
+    last_name,
+    salary,
+    salary * 1.15
+FROM employees;
+
+SELECT DISTINCT department_id
+FROM employees;
+
+-- Concatenation.
+SELECT first_name || ' ' || last_name "Nome Completo"
+FROM employees;
