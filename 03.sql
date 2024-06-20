4bb95bc8ad8f3a6388737400c796b4f4969aebad
Valmir <vmcfilho@gmail.com>
2024-06-20 20:13:56 -0300

    Create 03.sql

---
+++ 03.sql
@@ -0,0 +1,20 @@
+-- Single row functions.
+SELECT
+    employee_id, last_name, department_id
+FROM
+    employees
+WHERE UPPER(last_name) = 'KING';
+
+SELECT
+    employee_id, last_name, department_id
+FROM
+    employees
+WHERE LOWER(last_name) = 'king';
+
+SELECT
+    employee_id, last_name, department_id
+FROM
+    employees
+WHERE INITCAP(last_name) = 'King'; -- First capital letter.
+
+SELECT SYSDATE-3 FROM DUAL;
