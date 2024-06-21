a49c130b0d54ca1ebfb2d5bdce805c3c02284270
Valmir <vmcfilho@gmail.com>
2024-06-21 20:24:23 -0300

    Update 04.sql

--- 04.sql
+++ 04.sql
@@ -8,3 +8,15 @@ FROM employees;
 SELECT COUNT(location_id)
 FROM departments
 WHERE location_id = 1700;
+
+SELECT
+    department_id, AVG(salary)
+FROM employees
+GROUP BY department_id
+ORDER BY department_id;
+
+SELECT
+    department_id, job_id, SUM(salary)
+FROM employees
+GROUP BY department_id, job_id
+ORDER BY department_id, job_id;
