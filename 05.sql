913c77c7e53477d47875ae397aaf7022de09349f
Valmir <vmcfilho@gmail.com>
2024-06-22 16:08:13 -0300

    Update 05.sql

--- 05.sql
+++ 05.sql
@@ -1,10 +1,23 @@
 -- Joins.
 SELECT
-    employee_id,
-    first_name,
-    last_name,
-    department_name
+    e.employee_id,
+    e.first_name,
+    e.last_name,
+    d.department_name
 FROM employees e
-JOIN departments d
+JOIN departments d -- INNER JOIN.
 ON e.department_id = d.department_id
 ORDER BY first_name;
+
+SELECT
+    e.employee_id,
+    j.job_title,
+    d.department_name,
+    l.city,
+    l.state_province,
+    l.country_id
+FROM employees e
+JOIN jobs j ON e.job_id = j.job_id
+JOIN departments d ON e.department_id = d.department_id
+JOIN locations l ON d.location_id = l.location_id
+ORDER BY e.employee_id;
