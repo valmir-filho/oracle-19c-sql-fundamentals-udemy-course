91cf5952fb60508120d73037a7cd3ac178f3961a
Valmir <vmcfilho@gmail.com>
2024-06-23 19:01:45 -0300

    Update 05.sql

--- 05.sql
+++ 05.sql
@@ -21,3 +21,29 @@ JOIN jobs j ON e.job_id = j.job_id
 JOIN departments d ON e.department_id = d.department_id
 JOIN locations l ON d.location_id = l.location_id
 ORDER BY e.employee_id;
+
+SELECT
+    e.first_name,
+    e.last_name,
+    d.department_id,
+    d.department_name
+FROM employees e LEFT JOIN departments d ON e.department_id = d.department_id;
+
+SELECT
+    e.first_name,
+    e.last_name,
+    d.department_id,
+    d.department_name
+FROM employees e RIGHT JOIN departments d ON e.department_id = d.department_id;
+
+SELECT
+    e.first_name,
+    e.last_name,
+    d.department_id,
+    d.department_name
+FROM employees e FULL JOIN departments d ON e.department_id = d.department_id;
+
+SELECT
+    last_name,
+    department_name
+FROM employees CROSS JOIN departments;
